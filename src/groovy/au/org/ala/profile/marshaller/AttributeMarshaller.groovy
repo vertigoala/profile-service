250f40fc4e5934f4fdc82dd9365568c0d62a8c13
m-r-c <m-r-c@users.noreply.github.com>
2015-05-07 09:29:50 +1000

    Adds a plain-text version of the attribute text to the marshalled object

--- src/groovy/au/org/ala/profile/marshaller/AttributeMarshaller.groovy
+++ src/groovy/au/org/ala/profile/marshaller/AttributeMarshaller.groovy
@@ -4,6 +4,8 @@ import au.org.ala.profile.Opus
 import au.org.ala.profile.Profile
 import grails.converters.JSON
 
+import static org.apache.commons.lang3.StringEscapeUtils.unescapeHtml4
+
 class AttributeMarshaller {
 
     void register() {
@@ -13,6 +15,7 @@ class AttributeMarshaller {
                     title: "${attr.title.name}",
                     order: "${attr.title.order}",
                     text: "${attr.text}",
+                    plainText: cleanupText(attr.text),
                     creators: attr.creators.collect{ it.name },
                     editors: attr.editors.collect{ it.name },
                     original: attr.original,
@@ -36,4 +39,12 @@ class AttributeMarshaller {
         ]
     }
 
+    def cleanupText(str) {
+        if (str) {
+            str = unescapeHtml4(str)
+            // preserve line breaks as new lines, remove all other html
+            str = str.replaceAll(/<p\/?>|<br\/?>/, "\n").replaceAll(/<.+?>/, "").trim()
+        }
+        return str
+    }
 }
