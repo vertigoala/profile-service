4f0166069fe4ec581eb6b55b4f43cb0d14628cfe
temi <temivarg@gmail.com>
2015-07-03 09:27:52 +1000

    adding documentation

--- grails-app/services/au/org/ala/profile/ReportService.groovy
+++ grails-app/services/au/org/ala/profile/ReportService.groovy
@@ -2,8 +2,18 @@ package au.org.ala.profile
 
 class ReportService {
 
+    /**
+     * get the most recent change between two dates for an opus.
+     * used to generate reports
+     * @param from
+     * @param to
+     * @param opus
+     * @return
+     */
     Map mostRecentChange(Date from, Date to, Opus opus) {
 
+        // check if attributes are updated since attribute update does not change
+        // profile's lastUpdated attribute
         List attributes = Attribute.withCriteria {
             ge('lastUpdated', from)
             le('lastUpdated', to)
@@ -11,11 +21,14 @@ class ReportService {
                 distinct('profile')
             }
         };
+
+        //get the identifier
         List profileIds = [];
         attributes.each {
             profileIds.push(it.uuid);
         };
 
+        //get profiles updated or profiles with the given ids
         List profiles = Profile.withCriteria{
             eq('opus', opus)
             and {
