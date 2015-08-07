76af10641be2d395fe83156ce7c50d6ce9f6d1b3
ben3000 <ben.richardson@dpaw.wa.gov.au>
2015-08-07 14:18:36 +0800

    StatisticsController now uses ReportService to generate a "Updates This Month" statistic, which assumes a month is 30 days, as is the case with the Recent Updates report; each statistic now has an id property for future i18n support.

--- grails-app/controllers/au/org/ala/profile/StatisticsController.groovy
+++ grails-app/controllers/au/org/ala/profile/StatisticsController.groovy
@@ -1,11 +1,11 @@
 package au.org.ala.profile
-
 import au.org.ala.profile.security.Role
 import grails.converters.JSON
 
 class StatisticsController extends BaseController {
 
 	StatisticsService statisticsService
+	ReportService reportService
 
 	def index() {
 		if (!params.opusId) {
@@ -22,6 +22,7 @@ class StatisticsController extends BaseController {
 					eq("opus", opus)
 				}
 				statistics.add([
+						id: 'profileCount',
 				        name: 'Profiles',
 						value: profileCount
 				])
@@ -30,24 +31,37 @@ class StatisticsController extends BaseController {
 					eq("role", Role.ROLE_PROFILE_EDITOR)
 				}
 				statistics.add([
+						id: 'editorCount',
 				        name: 'Editors',
 						value: editorCount
 				])
 
+				Date from = new Date().minus(30)
+				Date to = new Date()
+				Map updatesThisMonth = reportService.recentUpdates(opus.uuid, from, to, 0, 1, true)
+				statistics.add([
+						id: 'updatesThisMonth',
+						name: 'Updates This Month',
+						value: updatesThisMonth.recordCount
+				])
+
 				Profile profile = statisticsService.lastEditedProfile(opus)
 				statistics.add([
+						id: 'lastEditedProfile',
 				        name: 'Last Edited',
 						value: "${profile.scientificName} by ${profile.lastUpdatedBy} on ${profile.lastUpdated.format("dd MMM yyyy")}"
 				])
 
 				int namesNotInNSL = statisticsService.profilesWithNameNotInNSL(opus)
 				statistics.add([
+						id: 'namesNotInNSL',
 				        name: 'Names Not In NSL',
 						value: namesNotInNSL
 				])
 
 				float namesNotInNSLAsPercent = statisticsService.profilesWithNameNotInNSLAsPercent(opus)
 				statistics.add([
+						id: 'namesNotInNSLPercent',
 				        name: 'Names Not In NSL Percent',
 						value: namesNotInNSLAsPercent,
 				])
