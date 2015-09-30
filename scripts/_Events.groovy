b8829edef131ca26e723dc14b8c720c623385efd
m-r-c <m-r-c@users.noreply.github.com>
2015-09-30 11:56:20 +1000

    Adds build timestamp to the application.properties file when the WAR is created

---
+++ scripts/_Events.groovy
@@ -0,0 +1,5 @@
+eventCompileStart = { warName, stagingDir ->
+    ant.propertyfile(file: "${stagingDir}/WEB-INF/classes/application.properties") {
+        entry(key: "app.build", value: new Date().format("dd/MM/yyyy HH:mm:ss"))
+    }
+}
