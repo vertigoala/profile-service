eefd0a2caf597e86e8ddd04343897dc363a9c3c1
m-r-c <m-r-c@users.noreply.github.com>
2015-12-18 17:48:49 +1100

    NT import (first pass); util for inspecting name matching (in progress)

---
+++ scripts/NTImport.groovy
@@ -0,0 +1,201 @@
+@Grab(group = 'org.codehaus.groovy.modules.http-builder', module = 'http-builder', version = '0.7')
+@Grab('org.apache.commons:commons-lang3:3.3.2')
+@Grab('com.xlson.groovycsv:groovycsv:1.0')
+
+import groovyx.net.http.RESTClient
+import static com.xlson.groovycsv.CsvParser.parseCsv
+
+import static org.apache.commons.lang3.StringEscapeUtils.unescapeHtml4
+
+import static groovyx.net.http.ContentType.JSON
+
+class NTImport {
+
+    static void main(args) {
+        def cli = new CliBuilder(usage: "groovy NTImport -d <datadir> -o opusId -p <profileServiceBaseUrl>")
+        cli.d(longOpt: "dir", "source data dir", required: true, args: 1)
+        cli.o(longOpt: "opusId", "UUID of the NSW Flora Opus", required: true, args: 1)
+        cli.p(longOpt: "profileServiceBaseUrl", "Base URL of the profile service", required: true, args: 1)
+
+        OptionAccessor opt = cli.parse(args)
+
+        if (!opt) {
+            cli.usage()
+            return
+        }
+
+        String OPUS_ID = opt.o
+        String DATA_FILE = opt.d
+        String REPORT_FILE = "report.txt"
+        String PROFILE_SERVICE_IMPORT_URL = "${opt.p}/import/profile"
+        String PROFILE_SERVICE_REPORT_URL = "${opt.p}/"
+
+        List profiles = []
+
+        println "Processing file..."
+        int count = 0
+
+        Map<String, List<Integer>> scientificNames = [:]
+        Map<Integer, String> invalidLines = [:]
+
+        File report = new File(REPORT_FILE)
+        if (report.exists()) {
+            report.delete()
+            report.createNewFile()
+        }
+
+        List<File> files = [new File("${DATA_FILE}/vol1.csv"), new File("${DATA_FILE}/vol2.csv")]
+
+        List<String> attributeColumns = ["Description",
+                                         "Diagnostic Characters",
+                                         "Similar Taxa",
+                                         "Habitat",
+                                         "Phenology Flowering",
+                                         "Phenology Fruiting",
+                                         "Scope Geography",
+                                         "Distribution International",
+                                         "Distribution International",
+                                         "Flora Information Notes",
+                                         "Specimens",
+                                         "Chromosome",
+                                         "Licence",
+                                         "Taxonomic Literature",
+                                         "Etymology",
+                                         "Seed Collection",
+                                         "Seed Treatment",
+                                         "Seed Propagation General",
+                                         "Seed Propagation General",
+                                         "Cutting Collection",
+                                         "Cutting Treatment",
+                                         "Cutting Propagation General",
+                                         "Division Collection",
+                                         "Division Propagation General",
+                                         "Grafting Information"
+        ]
+
+        files.each {
+            println "Processing file ${it.getName()}..."
+
+            def csv = parseCsv(new FileReader(it))
+            csv.each { fields ->
+                if (count++ % 50 == 0) println "Processing line ${count}..."
+
+                String taxonName = fields["Taxon Name"]
+                if (taxonName.split(" ").length > 1) {
+                    taxonName = taxonName.substring(taxonName.indexOf(" ")).trim()
+                }
+
+                taxonName = taxonName.cha
+
+                List attributes = []
+
+                attributeColumns.each {
+                    String value = clean(fields[it])
+                    if (value && value != "-") {
+                        attributes << [title: it, text: value, stripHtml: false]
+                    }
+                }
+
+                if (!scientificNames.containsKey(taxonName)) {
+                    Map profile = [scientificName              : taxonName,
+                                   nslNomenclatureMatchStrategy: "APC_OR_LATEST",
+                                   attributes                  : attributes,
+                                   fullName                    : taxonName]
+
+                    profiles << profile
+                }
+
+                scientificNames.get(taxonName, []) << count
+            }
+        }
+
+        Map opus = [
+                opusId  : OPUS_ID,
+                profiles: profiles
+        ]
+
+        println "Importing..."
+        def service = new RESTClient(PROFILE_SERVICE_IMPORT_URL)
+
+        def resp = service.post(body: opus, requestContentType: JSON)
+
+        String importId = resp.data.id
+
+        println "Import report will be available at ${PROFILE_SERVICE_REPORT_URL}import/${importId}/report"
+
+        int sleepTime = 5 * 60 * 1000
+        println "${new Date().format("HH:mm:ss.S")} Waiting for import to complete..."
+        Thread.sleep(sleepTime)
+
+        service = new RESTClient("${PROFILE_SERVICE_REPORT_URL}import/${importId}/report")
+        resp = service.get([:]).data
+
+        while (resp.status == "IN_PROGRESS") {
+            println "${new Date().format("HH:mm:ss.S")} Waiting for import to complete..."
+            Thread.sleep(sleepTime)
+
+            resp = service.get([:]).data
+        }
+
+        if (invalidLines) {
+            report << "Invalid lines from source file:\n"
+            invalidLines.each { k, v ->
+                report << "\tLine ${k}: ${v}\n"
+            }
+        }
+
+        if (scientificNames.any { k, v -> v.size() > 1 && k != null }) {
+            report << "\n\nDuplicate scientific names (only the first record will be imported): \n"
+            scientificNames.each { k, v ->
+                if (v.size() > 1 && k) {
+                    report << "\t${k}, on lines ${v}. Line ${v.first()} was imported.\n"
+                }
+            }
+        }
+
+        int success = 0
+        int failed = 0
+        int warnings = 0
+        report << "\n\nImport results: \n"
+        report << "\nStarted: ${resp.report.started}"
+        report << "\nFinished: ${resp.report.finished}"
+
+        resp.report.profiles.each { k, v ->
+            if (v.status.startsWith("success")) {
+                success++
+            } else if (v.status.startsWith("warning")) {
+                warnings++
+            } else {
+                failed++
+            }
+        }
+
+        report << "\n\nImported ${success} of ${count} profiles with ${failed} errors and ${warnings} warnings\n\n"
+
+        resp.report.profiles.each { k, v ->
+            if (v.status.startsWith("warning")) {
+                report << "\t${k} succeeded with ${v.warnings.size()} warnings:\n"
+                v.warnings.each {
+                    report << "\t\t${it}\n"
+                }
+            } else if (v.status.startsWith("error")) {
+                report << "\t${k} failed with ${v.errors.size()} errors and ${v.warnings.size()} warnings:\n"
+                report << "\t\tWarnings\n"
+                v.warnings.each {
+                    report << "\t\t\t${it}\n"
+                }
+                report << "\t\tErrors\n"
+                v.errors.each {
+                    report << "\t\t\t${it}\n"
+                }
+            }
+        }
+
+        println "Import finished. See ${report.absolutePath} for details"
+    }
+
+    static String clean(String str) {
+        unescapeHtml4(str)
+    }
+
+}
---
+++ scripts/NameCheck.groovy
@@ -0,0 +1,265 @@
+@Grab('com.xlson.groovycsv:groovycsv:1.0')
+
+@GrabResolver(name = 'ala', root = 'http://nexus.ala.org.au/content/groups/public/')
+@Grab('au.org.ala:ala-name-matching:2.1')
+@GrabExclude('org.apache.lucene:lucene-queries')
+
+import au.org.ala.names.model.LinnaeanRankClassification
+import au.org.ala.names.model.NameSearchResult
+import au.org.ala.names.search.ALANameSearcher
+import au.org.ala.names.search.HomonymException
+import au.org.ala.names.search.ParentSynonymChildException
+import groovy.json.JsonSlurper
+
+import java.util.concurrent.atomic.AtomicInteger
+
+import static com.xlson.groovycsv.CsvParser.parseCsv
+import static groovyx.gpars.GParsPool.withPool
+import static org.apache.commons.lang3.StringEscapeUtils.unescapeHtml4
+
+class NameMatch {
+    static ALANameSearcher currentALANames = new ALANameSearcher("/data/lucene/namematching13")
+
+    static main(String[] args) {
+        def cli = new CliBuilder(usage: "groovy NameCheck -a -n <name>")
+        cli.a(longOpt: "matchAll", "match all NSL names", required: false, args: 0)
+        cli.n(longOpt: "name", "match this name", required: false, args: 1)
+        cli.l(longOpt: "nsl", "match this name against the NSL as well as ALA", required: false, args: 0)
+        cli.u(longOpt: "umatchedNSL", "run the ALA unmatched names against the NSL", required: false, args: 0)
+
+        OptionAccessor opt = cli.parse(args)
+
+        if (!opt) {
+            cli.usage()
+            return
+        }
+
+        String name = opt.name
+        boolean matchAll = opt.a as boolean
+        boolean matchNameToNSL = opt.l as boolean
+        boolean umatchedNSL = opt.u as boolean
+
+        if (matchAll) {
+            matchAllNslNames()
+        } else if (umatchedNSL) {
+            runUnmatchedALANamesAgainstNSL()
+        } else {
+            matchALAName(name, true)
+            if (matchNameToNSL) {
+                matchNSLName(name)
+            }
+        }
+    }
+
+    static runUnmatchedALANamesAgainstNSL() {
+        long start = System.currentTimeMillis()
+
+        def csv = parseCsv(new FileReader("/data/nslnames/unmatched.csv"))
+
+        List nslMatch = Collections.synchronizedList([])
+        List nslNoMatch = Collections.synchronizedList([])
+
+        AtomicInteger i = new AtomicInteger(0)
+//        withPool(1) {
+            csv.each { fields ->
+                i.incrementAndGet()
+
+                String name = fields.NSLName
+
+                Map match = matchNSLName(name)
+                if (match) {
+                    nslMatch << match.fullName
+                } else {
+                    nslNoMatch << name
+                }
+//            }
+        }
+
+        long finish = System.currentTimeMillis()
+        println "Processed ${i} names in ${finish - start} millis - Matches: ${nslMatch.size()}; No match: ${nslNoMatch.size()}"
+    }
+
+    static matchAllNslNames() {
+        long start = System.currentTimeMillis()
+
+        File nslSimpleNames = new File("/data/nslnames/nslsimplename-2015-12-18-4237.csv")
+
+        println "Reading CSV file ${nslSimpleNames.getAbsolutePath()}"
+        def csv = parseCsv(new FileReader(nslSimpleNames))
+
+        String targetName = "full_name_html"
+
+        List matches = Collections.synchronizedList([])
+        List exactMatches = Collections.synchronizedList([])
+        List unmatched = Collections.synchronizedList([])
+        List nameSameAuthorDifferent = Collections.synchronizedList([])
+        List nameDifferent = Collections.synchronizedList([])
+        List failures = Collections.synchronizedList([])
+
+        println "Processing..."
+        AtomicInteger i = new AtomicInteger(0)
+        AtomicInteger hybrids = new AtomicInteger(0)
+        AtomicInteger noNsl = new AtomicInteger(0)
+
+        withPool(10) {
+            csv.eachParallel { fields ->
+                if (i.incrementAndGet() % 100 == 0) {
+                    println "Processing ${i}..."
+                }
+
+                String nslName = removeHtml(fields[targetName])
+                String nslSimpleName = removeHtml(fields.simple_name_html)
+                String cleanedNsl = cleanupText(nslName)
+
+                if (fields.hybrid == 't') {
+                    hybrids.incrementAndGet()
+                }
+                if (nslName && cleanedNsl) {
+
+                    List<String> outputs = [nslName]
+
+                    Map matchedName = matchALAName(nslName)
+                    outputs << matchedName.fullName
+
+
+                    String line = outputs.join(",") + "\n"
+                    if (!matchedName) {
+                        unmatched << line
+                    } else if (matchedName && !matchedName.exception) {
+                        matches << line
+
+                        String cleanedMatch = matchedName.clean?.fullName
+
+                        if (cleanedNsl == cleanedMatch) {
+                            exactMatches << line
+                        } else {
+                            if (matchedName.clean?.fullName?.startsWith(nslSimpleName)) {
+                                nameSameAuthorDifferent << line
+                            } else {
+                                nameDifferent << line
+                            }
+                        }
+                    } else {
+                        failures << line
+                    }
+
+
+                } else {
+                    noNsl.incrementAndGet()
+                }
+            }
+        }
+
+        writeToFile("unmatched", unmatched)
+        writeToFile("nameDifferent", nameDifferent)
+        writeToFile("nameSameAuthorDifferent", nameSameAuthorDifferent)
+
+        println "hybrids: ${hybrids}"
+        long finish = System.currentTimeMillis()
+        println """Processed ${i.intValue()} names in ${finish - start} millis:
+                   Failures: ${failures.size()};
+                   Total Matches: ${matches.size()};
+                        Exact Matches: ${exactMatches.size()};
+                        Name mismatch: ${nameDifferent.size()};
+                        Author mismatch: ${nameSameAuthorDifferent.size()};
+                   No match: ${unmatched.size()}
+                   Empty ${targetName}: ${noNsl.intValue()}"""
+    }
+
+    static writeToFile(String name, List rows) {
+        File file = new File("/data/nslnames/${name}.csv")
+        if (file.exists()) {
+            file.delete()
+            file.createNewFile()
+        }
+        file << "NSLName, ALAName\n"
+
+        rows.each {
+            file << it
+        }
+    }
+
+    static String cleanupText(String text) {
+        text ? text.replaceAll(/[\.,"'\(\)\-]/, "").replaceAll(" +", " ").trim() : ""
+    }
+
+    static Map matchALAName(String name, boolean print = false) {
+        Map match = [:]
+
+        if (!name) {
+            return match
+        }
+
+        try {
+            LinnaeanRankClassification rankClassification = new LinnaeanRankClassification()
+            rankClassification.setScientificName(name)
+//            rankClassification.setKingdom("Plantae")
+
+            NameSearchResult result = currentALANames.searchForAcceptedRecordDefaultHandling(rankClassification, true, true)
+
+            if (print) {
+                println "ALA Match: ${result}\n"
+            }
+
+            if (result && result.getRankClassification()) {
+                match.clean = [:]
+                match.scientificName = result.getRankClassification().getScientificName()
+                match.clean.scientificName = cleanupText(result.getRankClassification().getScientificName())
+                match.author = result.getRankClassification().getAuthorship()
+                match.clean.author = cleanupText(result.getRankClassification().getAuthorship())
+                match.fullName = "${result.getRankClassification().getScientificName()} ${result.getRankClassification().getAuthorship() ?: ""}".trim()
+                match.clean.fullName = cleanupText("${result.getRankClassification().getScientificName()} ${result.getRankClassification().getAuthorship() ?: ""}".trim())
+            } else {
+                List<NameSearchResult> matches = currentALANames.searchForRecords(name, null, rankClassification, 100, true, true)
+                println matches
+                if (matches?.size() > 1) {
+                    match.alternatives = matches.collect { "${it.rank.rank}: ${it?.rankClassification?.scientificName}" }.join(", ")
+                }
+            }
+        } catch (HomonymException e) {
+            e.results?.each { println it }
+        } catch (ParentSynonymChildException e) {
+            e.results?.each { println it }
+        } catch (Exception e) {
+            match.exception = e.message
+            e.printStackTrace()
+        }
+
+        match
+    }
+
+    static Map matchNSLName(String name) {
+        Map match = [:]
+        try {
+            String resp = new URL("https://biodiversity.org.au/nsl/services/api/name/acceptable-name.json?name=\"${URLEncoder.encode(name, "utf-8")}\"").text
+            def json = new JsonSlurper().parseText(resp)
+            println "NSL Match: ${json}\n"
+            if (json.count == 1) {
+                match.scientificName = json.names[0].simpleName
+                match.scientificNameHtml = json.names[0].simpleNameHtml
+                match.fullName = json.names[0].fullName
+                match.fullNameHtml = json.names[0].fullNameHtml
+                match.nameAuthor = match.fullNameHtml
+                String linkUrl = json.names[0]._links.permalink.link
+                match.nslIdentifier = linkUrl.substring(linkUrl.lastIndexOf("/") + 1)
+                match.nslProtologue = json.names[0].primaryInstance[0]?.citationHtml
+            } else {
+                println("${json.count} NSL matches for ${name}")
+            }
+        } catch (Exception e) {
+            match.exception = e.message
+            println e.message
+        }
+
+        match
+    }
+    static removeHtml(str) {
+        if (str) {
+            str = unescapeHtml4(str)
+            // preserve line breaks as new lines, remove all other html
+            str = str.replaceAll(/<p\/?>|<br\/?>/, "\n").replaceAll(/<.+?>/, "").replaceAll(" +", " ").trim()
+        }
+        return str
+    }
+
+}
