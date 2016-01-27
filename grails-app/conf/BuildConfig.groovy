ed24a59114719aac18a70d87fe1e03a64b6b55fe
m-r-c <m-r-c@users.noreply.github.com>
2016-01-27 16:12:41 +1100

    Excludes groovy-all dependency from elasticsearch plugin

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -55,9 +55,11 @@ grails.project.dependency.resolution = {
         build ":release:3.0.1"
         build ":tomcat:7.0.55"
         runtime ":ala-auth:1.3.1"
-        runtime ":ala-ws-security:1.0"
+        runtime ":ala-ws-security:1.1"
         runtime ":mongodb:3.0.3"
-        runtime ":elasticsearch:0.0.4.6"
+        runtime (":elasticsearch:0.0.4.6") {
+            excludes 'groovy-all'
+        }
         runtime ":cors:1.1.6"
         compile ":quartz:1.0.2"
         compile ":mail:1.0.7"
