01ff5ce2556e533ed6e91ec4cc71a2df8719f73c
Simon Bear <simon.bear@csiro.au>
2016-02-19 17:35:47 +1100

    Sanitize bibliography as well

--- grails-app/domain/au/org/ala/profile/Bibliography.groovy
+++ grails-app/domain/au/org/ala/profile/Bibliography.groovy
@@ -1,5 +1,6 @@
 package au.org.ala.profile
 
+import au.org.ala.profile.sanitizer.SanitizedHtml
 import groovy.transform.EqualsAndHashCode
 import groovy.transform.ToString
 
@@ -8,6 +9,7 @@ import groovy.transform.ToString
 class Bibliography {
 
     String uuid
+    @SanitizedHtml
     String text
     int order
 
