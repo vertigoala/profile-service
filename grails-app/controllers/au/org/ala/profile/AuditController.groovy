b09bd1ca4012f459e6942aefd3915f9047ff27fb
chrisala <chris.godwin.ala@gmail.com>
2016-03-01 15:37:55 +1100

    Added pagination to the profile audit function.  #177

--- grails-app/controllers/au/org/ala/profile/AuditController.groovy
+++ grails-app/controllers/au/org/ala/profile/AuditController.groovy
@@ -2,16 +2,27 @@ package au.org.ala.profile
 
 import au.ala.org.ws.security.RequireApiKey
 import grails.converters.JSON
+import grails.gorm.PagedResultList
 
 @RequireApiKey
 class AuditController {
+
+    static Integer DEFAULT_PAGE_SIZE = 100
+    static Integer DEFAULT_OFFSET = 0
 
     def index() {}
 
     def auditTrailForObject() {
         log.debug("Finding audit trail for entity ${params.entityId}")
-        def results = AuditMessage.findAllByEntityId(params.entityId, [sort: "date", order: "desc", offset: 0])
-        render results as JSON
+        Integer offset = params.getInt('offset', DEFAULT_OFFSET)
+        Integer max = params.getInt('max', DEFAULT_PAGE_SIZE)
+
+        PagedResultList results = AuditMessage.createCriteria().list(max: max, offset:offset) {
+            eq('entityId', params.entityId)
+            order("date", "desc")
+        }
+        Map response = [total: results.totalCount, items: results]
+        render response as JSON
     }
 
     def auditTrailForUser() {
