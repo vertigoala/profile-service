31d08efcca19cd849ffdf5f9bdfdb006e3112566
Simon Bear <simon.bear@csiro.au>
2017-06-08 19:04:14 +1000

    AtlasOfLivingAustralia/profile-hub#547 Filter UI - User selects list for filter  - Fix integration tests

--- grails-app/domain/au/org/ala/profile/Opus.groovy
+++ grails-app/domain/au/org/ala/profile/Opus.groovy
@@ -79,6 +79,7 @@ class Opus {
 
     List<String> additionalStatuses = ['In Review', 'Complete']
 
+    static transients = ['profileCount', 'florulaListId']
     static hasMany = [additionalOccurrenceResources: OccurrenceResource, authorities: Authority, tags: Tag]
     static embedded = ['supportingOpuses', 'sharingDataWith', 'attachments', 'brandingConfig', 'mapConfig', 'profileLayoutConfig', 'dataResourceConfig', 'opusLayoutConfig']
 
