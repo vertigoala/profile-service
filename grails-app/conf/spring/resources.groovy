c50f5837962891d7afc3e61d0d9c6dd3c699cf26
Simon Bear <simon.bear@csiro.au>
2017-09-29 15:10:46 +1000

    Remove unused import

--- grails-app/conf/spring/resources.groovy
+++ grails-app/conf/spring/resources.groovy
@@ -1,7 +1,6 @@
 import au.org.ala.profile.listener.AuditListener
 import au.org.ala.profile.marshaller.*
 import au.org.ala.profile.sanitizer.SanitizerPolicy
-import org.grails.datastore.mapping.core.Datastore
 
 // Place your Spring DSL code here
 beans = {
