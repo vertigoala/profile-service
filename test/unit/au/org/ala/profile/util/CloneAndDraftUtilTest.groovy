91c656ae9cfc605dda58c4695d2b89e60f2b62ad
Simon Bear <simon.bear@csiro.au>
2018-11-27 13:30:06 +1100

    Rename test

--- test/unit/au/org/ala/profile/util/CloneAndDraftUtilTest.groovy
+++ test/unit/au/org/ala/profile/util/CloneAndDraftUtilTest.groovy
@@ -165,7 +165,7 @@ class CloneAndDraftUtilTest extends Specification {
         draft.occurrenceQuery == original.occurrenceQuery
     }
 
-    def "publishShouldUpdateAllFields"() {
+    def "updateProfileFromDraftShouldUpdateAllFields"() {
         when:
         CloneAndDraftUtil.updateProfileFromDraft(profileWithDraft)
 
